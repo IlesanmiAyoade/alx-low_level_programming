f3392996f1b62263a52e25694b1508d4758afcb4
IlesanmiAyoade <ilesanmibenjaminay@gmail.com>
2023-06-18 16:49:07 -0700

    The commit adjust

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+   * main - print to screen
+   *
+   * Return: zero
+   */
+int main(void)
+{
+/**	char Mystr[] = "\"Programming is like building a multilingual puzzle";
+*/
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
